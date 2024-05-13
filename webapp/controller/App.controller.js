a56d443ec5849434eb019ab86b7b9654d52a7a23
allankruger <allanfloreskruger@gmail.com>
2024-05-13 02:23:49 +0200

    feat: add locales

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,23 +3,36 @@ sap.ui.define(
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
   ],
-  function (Controller, MessageToast, JSONModel) {
+  function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
       onInit: function () {
         let oData = {
           recipient: {
-            name: "UI5",
+            name: "Allan Krüger",
           },
         };
 
         let oModel = new JSONModel(oData);
         this.getView().setModel(oModel);
+
+        let i18nModel = new ResourceModel({
+          bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+          supportedLocales: [""],
+          fallbackLocale: "",
+        });
+        this.getView().setModel(i18nModel, "i18n");
       },
       onShowHello: function () {
-        MessageToast.show("Hello there!");
+        let oBundle = this.getView().getModel("i18n").getResourceBundle();
+        let sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        let sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
       },
     });
   }
